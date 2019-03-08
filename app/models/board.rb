b77dac8307c76da38b77d1f20115dc5bfd2ac98e
Shandra <shandradev85@gmail.com>
2019-03-07 20:39:17 -0700

    fixed board model

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,7 +2,6 @@ class Board < ApplicationRecord
   belongs_to :user
 
   has_many :lists
-  class Board < ActiveRecord::Base
     validates :title, length: { minimum: 2 }
     validates :title, length: { maximum: 500 }
  
