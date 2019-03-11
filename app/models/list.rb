9c814c6ab7e7680c0fdeb7bc8cc6c4fa169fb3ae
Shandra <shandradev85@gmail.com>
2019-03-11 15:19:43 -0600

    fixed create list

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,7 @@ class List < ApplicationRecord
   belongs_to :board
   has_many :tasks
 
-  validates :task, presence: true, on: :save
-  validates :body, allow_blank: true, length: {maximum: 500}
+  validates :list_name, allow_blank: false, length: {maximum: 500}
+  validates :task_name, presence: true, on: :save
 
 end
