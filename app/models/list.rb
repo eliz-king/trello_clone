d8d9ebc9c66a758b7be9da1b0bc7eedc91bb2f87
Emily Johnson <emily.johnsonmsp@gmail.com>
2019-03-12 15:15:46 -0600

    add dependent destroy to models

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   belongs_to :board
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 
   validates :list_name, allow_blank: false, length: {maximum: 500}
 
